52d3103683572216e283f7fbfc5d37ad28c038e0
Allen Lawrence <blue_sky1997@live.com>
2018-01-17 10:43:57 +0800

    Added more descriptions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello World\n")
-	fmt.Println("Today, I installed PostgreSql and run Gitea successfully. I also finded out the correct usage of APIs")
-	fmt.Println("Learned the logic and usage of account system")
-	fmt.Println("Learned the usage of error handler and more mongoose operations")
-	fmt.Println("Learned the primary logics of auto-build tool Now. Installed Ubuntu 18.04 Server and Desktop version")
-	fmt.Println("Learned how now.js's archiver works")
-	fmt.Println("Learned package \"server\"")
-	fmt.Println("Learned exoframe - Auto deployment framework")
-	fmt.Println("Tried to run it successfully")
-	fmt.Println("Successfully deployed Sentry on AWS")
+	
 }
