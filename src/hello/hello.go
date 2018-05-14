b200d0a4dfe12d7166f80d5f5ba518abf26ff186
Allen Lawrence <blue_sky1997@live.com>
2018-05-14 21:12:48 +0800

    Added more descriptions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Printf("Hello World\n")
 	fmt.Println("Today, I installed PostgreSql and run Gitea successfully. I also finded out the correct usage of APIs")
 	fmt.Println("Learned the logic and usage of account system")
+	fmt.Println("Learned the usage of error handler and more mongoose operations")
 }
