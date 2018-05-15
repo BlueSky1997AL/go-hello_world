12fe493953a7ef5a1fb08acd2d126710ab066ef1
Allen Lawrence <blue_sky1997@live.com>
2018-05-15 09:21:45 +0800

    Added more descriptions

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -7,4 +7,5 @@ func main() {
 	fmt.Println("Today, I installed PostgreSql and run Gitea successfully. I also finded out the correct usage of APIs")
 	fmt.Println("Learned the logic and usage of account system")
 	fmt.Println("Learned the usage of error handler and more mongoose operations")
+	fmt.Println("Learned the primary logics of auto-build tool Now. Installed Ubuntu 18.04 Server and Desktop version")
 }
